eecea52bf22260919b34d0fe9d2ae1d71afdbf0f
GitHub Action <action@github.com>
2022-10-12 11:04:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/gerardnico/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gerardnico/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gerardnico.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gerardnico.intellijplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/gerardnico/intellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gerardnico/intellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.gerardnico.intellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gerardnico.intellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
